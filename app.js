689eafe45b9385701d259730534c90905f61af56
Bogdan Burcea <cryptopoke29@gmail.com>
2023-02-28 14:06:00 +0000

    google_api

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-function initMap() {
-    const input = document.getElementById("c_address");
-    const autocomplete = new google.maps.places.Autocomplete(input);
-    
-    autocomplete.addListener("place_changed", () => {
-      
-      const place = autocomplete.getPlace();
-
-        if (!place.geometry) {
-          // User entered the name of a Place that was not suggested and
-          // pressed the Enter key, or the Place Details request failed.
-          //window.alert("No details available for input: '" + place.name + "'");
-          swal("Please fill all the *Required fields","Click okay to continue", "warning");
-          return;
-      }
-    });
-}
+const center = { lat: 50.064192, lng: -130.605469 };
+// Create a bounding box with sides ~10km away from the center point
+const defaultBounds = {
+  north: center.lat + 0.1,
+  south: center.lat - 0.1,
+  east: center.lng + 0.1,
+  west: center.lng - 0.1,
+};
+const input = document.getElementById("c_address");
+const options = {
+  bounds: defaultBounds,
+  componentRestrictions: { country: "uk" },
+  fields: ["address_components", "geometry", "icon", "name"],
+  strictBounds: false,
+  types: ["establishment"],
+};
+const autocomplete = new google.maps.places.Autocomplete(input, options);
